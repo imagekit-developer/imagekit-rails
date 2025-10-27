cf861e49732c2d2f6b3d91cc2875446d9001851a
Manu Chaudhary <manu@imagekit.io>
2025-10-27 11:26:38 +0530

    Refactor Active Storage integration in ImageKit service to improve structure and load order

--- lib/imagekit-rails.rb
+++ lib/imagekit-rails.rb
@@ -4,13 +4,15 @@ require 'imagekit'
 require_relative 'imagekit/rails/version'
 require_relative 'imagekit/rails/configuration'
 require_relative 'imagekit/rails/helper'
-require_relative 'imagekit/rails/railtie' if defined?(Rails::Railtie)
 
 # Load Active Storage integration if ActiveStorage is available
 if defined?(ActiveStorage)
+  require_relative 'imagekit/rails/active_storage'
   require_relative 'imagekit/rails/active_storage/service'
   require_relative 'imagekit/rails/active_storage/attached_extensions'
 end
+
+require_relative 'imagekit/rails/railtie' if defined?(Rails::Railtie)
 
 module Imagekit
   module Rails
---
+++ lib/imagekit/rails/active_storage.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Imagekit
+  module Rails
+    # Active Storage integration for ImageKit
+    module ActiveStorage
+    end
+  end
+end
