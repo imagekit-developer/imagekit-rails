3716d21009ca54c388bc599183840e17ed2704ab
Manu Chaudhary <manu@imagekit.io>
2025-10-27 10:04:37 +0530

    Refactor ImageKit service loader for Active Storage integration

--- lib/active_storage/service/image_kit_service.rb
+++ lib/active_storage/service/image_kit_service.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
-# Active Storage service loader for ImageKit
-# This file allows Active Storage to autoload the ImageKit service
-# when configured with `service: ImageKit` in storage.yml
-
 require 'imagekit/rails/active_storage/service'
 
-module ActiveStorage
-  module Service
-    # Alias for ImageKit service to match Active Storage naming conventions
-    ImageKitService = Imagekit::Rails::ActiveStorage::Service
-  end
+# Active Storage service adapter for ImageKit
+class ActiveStorage::Service::ImageKitService < Imagekit::Rails::ActiveStorage::Service
 end
